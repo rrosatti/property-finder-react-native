59505dd614652152d580da7381f40c6489717df4
Rodrigo R. Galvao <rodrigorosattig@gmail.com>
2018-11-03 12:33:19 -0300

    Using Flexbox
    The Flexbox style was used, which deals with margins, paddings and color.
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

--- SearchPage.js
+++ SearchPage.js
@@ -18,6 +18,16 @@ export default class SearchPage extends Component<Props> {
         <Text style={styles.description}>
           Search by place-name or postcode.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            underlineColorAndroid={'transparent'}
+            style={styles.searchInput}
+            placeholder='Search via name or postcode' />
+          <Button
+            onPress={() => {}}
+            color='#48BBEC'
+            title='Go' />
+        </View>
       </View>
     );
   }
@@ -34,5 +44,21 @@ const styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center',
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC',
   },
 });
